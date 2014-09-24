be420a3aad15fe82c05d6cf276a3ef1f0cd44200
Jazmine James <jazminemichelle@gmail.com>
2014-09-23 21:26:16 -0400

    implement quicksort partitioning, more unit tests

--- 2-quicksort/Quicksort.py
+++ 2-quicksort/Quicksort.py
@@ -14,5 +14,22 @@ def quicksort(array):
     return None
 
 
-def partition(array, pivot, min_idx, max_idx):
-    return None
+def partition_array(array, pivot_idx, min_idx, max_idx):
+    pivot = array[pivot_idx]
+    # move pivot elt to leftmost idx for now
+    array[min_idx], array[pivot_idx] = array[pivot_idx], array[min_idx]
+
+    # MUST maintain partitioned section:
+    # all elts A[min_idx + 1] to A[partition-1] < pivot and
+    # all elts A[partition] to A[current-1] > pivot.
+    partition = min_idx + 1
+    current = min_idx + 1
+
+    while current < max_idx:
+        if array[current] < pivot:
+            # rearrange so that maintain a partitioned section
+            array[partition], array[current] = array[current], array[partition]
+            partition += 1
+        current += 1
+
+    array[min_idx], array[partition-1] = array[partition-1], array[min_idx]
--- 2-quicksort/test/QuicksortTest.py
+++ 2-quicksort/test/QuicksortTest.py
@@ -16,21 +16,65 @@ class QuicksortTest(unittest.TestCase):
         result = Quicksort.quicksort(array)
         self.assertEqual(sorted_list, result)
 
-    def partition_helper(self, array, length, pivot, min_idx, max_idx):
+    def check_partition_valid(self, array, length, pivot, min_idx, max_idx):
+        pivot_idx = array.index(pivot)
         self.assertEqual(length, len(array))
-        self.assertEqual(0, len(filter(lambda x: x > pivot, array[min_idx:pivot])))
-        self.assertEqual(0, len(filter(lambda y: y < pivot, array[pivot:max_idx])))
+        self.assertEqual(0, len(filter(lambda x: x > pivot, array[min_idx:pivot_idx])))
+        self.assertEqual(0, len(filter(lambda y: y < pivot, array[pivot_idx:max_idx])))
 
     def test_partition_full(self):
         array = [9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0]
         pivot = 8
+        pivot_idx = array.index(pivot)
         init_len = len(array)
-        Quicksort.partition(array, pivot, 0, len(array))
-        self.partition_helper(array, init_len, pivot, 0, len(array))
+        Quicksort.partition_array(array, pivot_idx, 0, init_len)
+        self.check_partition_valid(array, init_len, pivot, 0, init_len)
+        self.assertEqual(pivot, array[pivot])
 
     def test_partition_subset(self):
         array = [9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0]
-        pivot = 4
+        pivot = 6
+        pivot_idx = array.index(pivot)
         init_len = len(array)
-        Quicksort.partition(array, pivot, 0, 8)
-        self.partition_helper(array, init_len, pivot, 0, 8)
+        Quicksort.partition_array(array, pivot_idx, 2, 8)
+        self.check_partition_valid(array, init_len, pivot, 2, 8)
+
+    def test_partition_partial_subset_left(self):
+        array = [9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0]
+        pivot = 8
+        pivot_idx = array.index(pivot)
+        init_len = len(array)
+        Quicksort.partition_array(array, pivot_idx, 0, 8)
+        self.check_partition_valid(array, init_len, pivot, 0, 8)
+
+    def test_partition_partial_subset_right(self):
+        array = [9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0]
+        pivot = 6
+        pivot_idx = array.index(pivot)
+        init_len = len(array)
+        Quicksort.partition_array(array, pivot_idx, 2, init_len)
+        self.check_partition_valid(array, init_len, pivot, 2, init_len)
+
+    def test_partition_with_dups(self):
+        array = [9, 12, 3, 2, 1, 6, 8, 2, 6]
+        pivot = 6
+        pivot_idx = array.index(pivot)
+        init_len = len(array)
+        Quicksort.partition_array(array, pivot_idx, 0, init_len)
+        self.check_partition_valid(array, init_len, pivot, 0, init_len)
+
+    def test_partition_min_elt_pivot(self):
+        array = [9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0]
+        pivot = 0
+        pivot_idx = array.index(pivot)
+        init_len = len(array)
+        Quicksort.partition_array(array, pivot_idx, 0, init_len)
+        self.check_partition_valid(array, init_len, pivot, 0, init_len)
+
+    def test_partition_max_elt_pivot(self):
+        array = [9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0]
+        pivot = 14
+        pivot_idx = array.index(pivot)
+        init_len = len(array)
+        Quicksort.partition_array(array, pivot_idx, 0, init_len)
+        self.check_partition_valid(array, init_len, pivot, 0, init_len)
